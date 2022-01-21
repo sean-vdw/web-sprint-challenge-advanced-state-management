6ac656e76fe80c30f9e053212c00b1399b2707af
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-01-21 08:34:37 -0800

    updated logger dependency to redux-logger

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import reducer from "./reducers";
+import { reducer } from "./reducers";
 
-// Middleware Dependencies
-import { logger } from "logger";
+// Middleware Packages
+import logger from "redux-logger";
 import thunk from 'redux-thunk';
 
 import "./index.css";
